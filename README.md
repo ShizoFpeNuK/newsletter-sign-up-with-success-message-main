db351aa840cec6a696f0b0bd51162af279cb223b
Shiza <vnchernovv02@gmail.com>
2023-09-18 21:49:30 +0300

    THE END

---
+++ README.md
@@ -0,0 +1,41 @@
+# Frontend Mentor - Newsletter sign-up form with success message solution
+
+This is a solution to the [Newsletter sign-up form with success message challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/newsletter-signup-form-with-success-message-3FC1AZbNrv).
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Add their email and submit the form
+- See a success message with their email after successfully submitting the form
+- See form validation messages if:
+  - The field is left empty
+  - The email address is not formatted correctly
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+![](./design/result/result.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- SCSS custom properties
+- Flexbox
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+- [TS](https://www.typescriptlang.org/) - Typed programming language that builds on JavaScript
+
+### What I learned
+
+During the project I learned to use forwardRef, dot-notation from React and some SCSS mixins.
--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,89 @@
 import styles from "./App.module.scss";
-import React, { FormEventHandler } from "react";
+import Button from "./components/Button/Button";
+import Input from "./components/Input/Input";
+import SuccessMessage from "./components/SuccessMessage/SuccessMessage";
+import { TYPES_INPUT } from "./helpers/reqexp/types_input";
+import { ChangeEventHandler, FC, FormEventHandler, useState } from "react";
 
-const App = () => {
+const App: FC = () => {
+	const [isSuccess, setIsSuccess] = useState<boolean>(false);
+	const [values, setValues] = useState<{ email: string }>({ email: "" });
+	// A crutch so that when you press a button, the input field is highlighted
+	const [isError, setIsError] = useState<boolean>(false);
+
+	const handleChange: ChangeEventHandler<HTMLInputElement> = ({ target: { value, name } }) => {
+		setValues({ ...values, [name]: value });
+	};
+
 	const onSubmit: FormEventHandler<HTMLFormElement> = (event) => {
 		event.preventDefault();
+
+		if (TYPES_INPUT.EMAIL.test(values.email)) {
+			setIsSuccess(true);
+			setValues({ email: "" });
+			setIsError(false);
+		} else {
+			setIsError(true);
+		}
+	};
+
+	const closeMessage = () => {
+		setIsSuccess(false);
 	};
 
 	return (
-		<div className={styles.app}>
-			<div className={styles.app__inner}>
-				<h1 className={styles.app__inner__title}>Stay updated!</h1>
+		<>
+			{!isSuccess ? (
+				<div className={styles.app}>
+					<>
+						<div className={styles.app__left}>
+							<h1 className={styles.app__left__title}>Stay updated!</h1>
 
-				<p className={styles.app__inner__text}>
-					Join 60,000+ product managers receiving monthly updates on:
-				</p>
+							<p className={styles.app__left__text}>
+								Join 60,000+ product managers receiving monthly updates on:
+							</p>
 
-				<ul className={styles.app__inner__list}>
-					<li>Product discovery and building what matters</li>
-					<li>Measuring to ensure updates are a success</li>
-					<li>And much more!</li>
-				</ul>
+							<ul className={styles.app__left__list}>
+								<li>Product discovery and building what matters</li>
+								<li>Measuring to ensure updates are a success</li>
+								<li>And much more!</li>
+							</ul>
 
-				<form
-					className={styles.app__inner__form}
-					onSubmit={onSubmit}
-				>
-					<label>
-						Email address{" "}
-						<input
-							type="email"
-							name="email"
-							autoComplete="on"
-							placeholder="email@company.com"
-						/>
-					</label>
+							<form
+								className={styles.app__left__form}
+								onSubmit={onSubmit}
+							>
+								<Input
+									error={isError}
+									type="email"
+									name="email"
+									title="Email address"
+									errorText="Valid email required"
+									placeholder="email@company.com"
+									onChange={handleChange}
+								/>
+								<Button type="submit">Subscribe to monthly newsletter</Button>
+							</form>
+						</div>
 
-					<button
-						className={styles.app__inner__form__submit}
-						type="submit"
-					>
-						Subscribe to monthly newsletter
-					</button>
-				</form>
-			</div>
-
-			<div>
-				<img
-					src="illustration-sign-up-desktop.svg"
-					alt="Picture"
-				/>
-			</div>
-
-			{/* <!-- Success message start --> */}
-			{/* Thanks for subscribing!
-
-      A confirmation email has been sent to ash@loremcompany.com.
-      Please open it and click the button inside to confirm your subscription.
-
-      Dismiss message */}
-			{/* <!-- Success message end --> */}
-		</div>
+						<div className={styles.app__right}>
+							<img
+              className={styles.img__desktop}
+								src="illustration-sign-up-desktop.svg"
+								alt="Picture"
+							/>
+              <img
+                className={styles.img__mobile}
+								src="illustration-sign-up-mobile.svg"
+								alt="Picture"
+							/>
+						</div>
+					</>
+				</div>
+			) : (
+				<SuccessMessage onClose={closeMessage} />
+			)}
+		</>
 	);
 };
 
